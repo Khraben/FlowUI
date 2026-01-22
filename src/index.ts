b52ce92beb9f746f81683d06fea6efe1b75d2c57
Khraben <kvn.jimenez.t@gmail.com>
2026-01-22 16:24:40 -0600

    update: clean index for the lib

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 export * from './app/components';
-export * from './app/constants';
+
+export {
+  BUTTON_VARIANTS,
+  BUTTON_SIZES,
+  BUTTON_ICON_POSITIONS,
+  BUTTON_ROUNDED_OPTIONS,
+  INPUT_VARIANTS,
+  INPUT_SIZES,
+  DATEPICKER_SIZES,
+} from './app/constants';
+
